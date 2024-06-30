3bd1a3e7cd55e98f54023c32e6e7be367e1b8f76
EgertJ <egertjaansoo@gmail.com>
2024-07-01 00:55:04 +0300

    deleted status

--- src/utils/statusToText.ts
+++
@@ -1,8 +0,0 @@
-import { OrderStatus } from "@prisma/client";
-
-export function statusToText(status: OrderStatus) {
-  if (status === "pending") return "Ootel";
-  if (status === "processing") return "Tegemisel";
-  if (status === "completed") return "Valmis";
-  if (status === "delivered") return "Kätte antud";
-}
